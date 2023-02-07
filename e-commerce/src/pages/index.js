9da5e42ff7d975cd6edbd938aaf28140150f6954
renzien <alifrizki1011@gmail.com>
2023-02-07 18:14:44 +0700

    add components

--- e-commerce/src/pages/index.js
+++ e-commerce/src/pages/index.js
@@ -1,9 +1,25 @@
 import React from 'react'
 
-const index = () => {
+import { Product, FooterBanner, HeroBanner } from 'components' 
+
+const Home = () => {
   return (
-    <div>index</div>
+    <>
+        HeroBanner
+
+        <div className="products-heading">
+            <h2>Best Selling All of Time!</h2>
+            <p>Check out our best selling products</p>
+        </div>
+
+        <div className="products-container">
+            {['Product 1', 'Product 2'].map((product) => product)}
+        </div>
+
+
+        Footer
+    </>
   )
 }
 
-export default index
+export default Home
